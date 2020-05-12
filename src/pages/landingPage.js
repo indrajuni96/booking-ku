72420411374ca23193f7d6443b6597e85fad774b
indrajuni96 <33251585+indrajuni96@users.noreply.github.com>
2020-05-12 12:27:44 +0700

    first commit

---
+++ src/pages/landingPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import modulename from "elements/button";
+
+export default class landingPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
