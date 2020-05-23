85ab056a94c0f98d6006d2b7cffd06632ed0e884
indrajuni96 <33251585+indrajuni96@users.noreply.github.com>
2020-05-23 19:41:22 +0700

    addedSetionMostPicked

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -4,13 +4,19 @@ import landingPageJson from "json/landingPage.json"
 
 import Header from "parts/header"
 import Hero from "parts/hero"
+import MostPicked from "parts/MostPicked"
 
 export default class landingPage extends Component {
+  constructor(props) {
+    super(props)
+    this.refMostPicked = React.createRef()
+  }
 
   render() {
     return <>
       <Header {...this.props}></Header>
-      <Hero data={landingPageJson.hero} />
+      <Hero refMostPicked={this.refMostPicked} data={landingPageJson.hero} />
+      <MostPicked refMostPicked={this.refMostPicked} data={landingPageJson.mostPicked} />
     </>;
   }
 }
